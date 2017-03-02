4586cf01f1881331f3c1d02b61bf613f645e7b86
Fanatpwnd <arcanis@mail.ru>
2017-03-02 13:56:36 +1200

    Correction

--- Levels/Loading.h
+++ Levels/Loading.h
@@ -3,9 +3,11 @@
 
 class Load: public Level
 {
+	sf::Sprite load;
+	sf::Texture tex;
 public:
-	void loadTextures(TextureManager* tex_manager){}
-	void draw(sf::RenderWindow* window){}
+	void loadTextures(TextureManager* tex_manager){ tex.loadFromFile("load.jpg"); load.setTexture(tex); load.setPosition(0,0);}
+	void draw(sf::RenderWindow* window){window->draw(load);}
 	void update(float time){}
 	void event(sf::Event* ev){}
 };
